79543522dff1ffd66d4f98fa3a1299616305fd97
rohan <advanirohan03@gmail.com>
2024-05-24 01:45:24 +0530

    offerings

--- app/(root)/offerings/page.tsx
+++ app/(root)/offerings/page.tsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Offerings = () => {
   return (
-    <div>Offerings</div>
+    <div className='min-h-screen flex-col flex'>
+    <div className="mt-[10rem] flex flex-col gap-10 px-[3rem] ">
+    <h1 className='text-2xl font-extrabold' >
+    AI Data Analysis
+    </h1>
+    <p className='text-lg font-medium'>
+    An Intelligent platform designed to transform raw data into actionable insights. Our AI Data Analyst is equipped with the latest machine learniing algorithms and cutting-edge technologies to analyze,interpret and visualize data with precision and speed 
+    </p>
+    </div>
+    <div className="flex flex-col gap-10 px-[3rem] ">
+    <h1 className='text-2xl font-extrabold' >
+    Gen AI
+    </h1>
+    <p className='text-lg font-medium'>
+    Gen AI Technology: Our platform features Gen AI technology, which allows users to generate custom AI models with ease, unleashing their creativity and innovation in data science projects.ke tax.
+    </p>
+    </div>
+  </div>
   )
 }
 
