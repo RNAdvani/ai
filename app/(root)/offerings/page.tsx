0ce8ea06262d7f311d460e762c0e6a2950a90ab7
rohan <advanirohan03@gmail.com>
2024-05-24 01:51:42 +0530

    updated

--- app/(root)/offerings/page.tsx
+++ app/(root)/offerings/page.tsx
@@ -2,15 +2,8 @@ import React from 'react'
 
 const Offerings = () => {
   return (
-    <div className='min-h-screen flex-col flex'>
-    <div className="mt-[10rem] flex flex-col gap-10 px-[3rem] ">
-    <h1 className='text-2xl font-extrabold' >
-    AI Data Analysis
-    </h1>
-    <p className='text-lg font-medium'>
-    An Intelligent platform designed to transform raw data into actionable insights. Our AI Data Analyst is equipped with the latest machine learniing algorithms and cutting-edge technologies to analyze,interpret and visualize data with precision and speed 
-    </p>
-    </div>
+    <div className='min-h-screen flex flex-col items-center justify-center'>
+        Offering
   </div>
   )
 }
